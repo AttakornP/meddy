8313be6af330430f49d200f8e16d2a50afb0a7e9
AttakornP <cartoonman_jr@hotmail.com>
2013-08-21 18:00:55 +0700

    add repos config for buildbot

--- buildbot_config/settings/settings.py
+++ buildbot_config/settings/settings.py
@@ -1,6 +1,6 @@
 BRANCH = "develop"
-MASTER_HOST = "buildbot_master_host"
-PROJECT_CODE_URL = "project_dirname"
+MASTER_HOST = "ec2-54-254-127-209.ap-southeast-1.compute.amazonaws.com"
+PROJECT_CODE_URL = "src"
 PROJECT_NAME = "meddy"
-REPOSITORY_URL = "/path/to/repo"
+REPOSITORY_URL = "https://github.com/AttakornP/meddy.git"
 
